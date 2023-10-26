2529ddcebc6d70a5473bf75e6db7ecabd3c31644
Антон Кулик <anton.smile.1998@gmail.com>
2023-10-26 21:10:59 +0300

    Удаление didChangeDependencies из coinScreen

--- lib/features/crypto_coin/view/crypto_coin_screen.dart
+++ lib/features/crypto_coin/view/crypto_coin_screen.dart
@@ -1,28 +1,38 @@
+import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:flutter/material.dart';
 
 class CryptoCoinScreen extends StatefulWidget {
-  const CryptoCoinScreen({super.key});
+  const CryptoCoinScreen({
+    super.key,
+    /*required this.coin*/
+  });
+
+  // final CryptoCoin coin;
 
   @override
   State<CryptoCoinScreen> createState() => _CryptoCoinScreenState();
 }
 
 class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
-  String? _coinName;
+  // TODO: Тут нужен блок!
 
   @override
-  void didChangeDependencies() {
-    final args = ModalRoute.of(context)?.settings.arguments;
-    assert(args != null && args is String, "You must provide String args ");
-    _coinName = args as String;
-    setState(() {});
-    super.didChangeDependencies();
+  void initState() {
+    super.initState();
   }
+  // @override
+  // void didChangeDependencies() {
+  //   final args = ModalRoute.of(context)?.settings.arguments;
+  //   assert(args != null && args is String, "You must provide String args ");
+  //   _coinName = args as String;
+  //   setState(() {});
+  //   super.didChangeDependencies();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(_coinName ?? "...")),
+      appBar: AppBar(title: Text("...")),
     );
   }
 }
