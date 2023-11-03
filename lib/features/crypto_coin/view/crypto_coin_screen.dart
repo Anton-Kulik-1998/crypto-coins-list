a95aa997cb2e02615a2a9d19594dad168359c845
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-03 23:33:28 +0300

    Вывод информации на экран coin

--- lib/features/crypto_coin/view/crypto_coin_screen.dart
+++ lib/features/crypto_coin/view/crypto_coin_screen.dart
@@ -1,5 +1,8 @@
+import 'package:crypto_coins_list/features/crypto_coin/bloc/crypto_coin_details/crypto_coins_details_bloc.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 class CryptoCoinScreen extends StatefulWidget {
   const CryptoCoinScreen({super.key, required this.coin});
@@ -12,9 +15,13 @@ class CryptoCoinScreen extends StatefulWidget {
 
 class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
   // TODO: Тут нужен блок!
+  final _coinDetailsBloc = CryptoCoinsDetailsBloc(
+    GetIt.I<AbstractCoinsRepository>(),
+  );
 
   @override
   void initState() {
+    _coinDetailsBloc.add(LoadCryptoCoinDetails(currencyCode: widget.coin.name));
     super.initState();
   }
   // @override
@@ -29,7 +36,35 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("${widget.coin.name}")),
+      appBar: AppBar(),
+      body: BlocBuilder<CryptoCoinsDetailsBloc, CryptoCoinsDetailsState>(
+        bloc: _coinDetailsBloc,
+        builder: (context, state) {
+          if (state is CryptoCoinDetailsLoaded) {
+            final coin = state.coin;
+            return Container(
+              child: Center(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 160,
+                    width: 160,
+                    child: Image.network(coin.fullImageUrl),
+                  ),
+                  const SizedBox(height: 24),
+                  Text(
+                    widget.coin.name,
+                    style: const TextStyle(
+                        fontSize: 26, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
