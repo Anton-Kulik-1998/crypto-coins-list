11030edc8ad74b0851ad668a09c20ad58bb72263
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-04 13:46:11 +0300

    Добавлены виджеты на экран coin

--- lib/features/crypto_coin/view/crypto_coin_screen.dart
+++ lib/features/crypto_coin/view/crypto_coin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:crypto_coins_list/features/crypto_coin/bloc/crypto_coin_details/crypto_coins_details_bloc.dart';
+import 'package:crypto_coins_list/features/crypto_coin/widgets/base_card.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -42,9 +43,8 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
         builder: (context, state) {
           if (state is CryptoCoinDetailsLoaded) {
             final coin = state.coin;
-            return Container(
-              child: Center(
-                  child: Column(
+            return Center(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   SizedBox(
@@ -56,15 +56,68 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
                   Text(
                     widget.coin.name,
                     style: const TextStyle(
-                        fontSize: 26, fontWeight: FontWeight.bold),
+                        fontSize: 26, fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  BaseCard(
+                    child: Center(
+                      child: Text(
+                        "${coin.priceInUSD} \$",
+                        style: const TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                  BaseCard(
+                    child: Column(
+                      children: [
+                        _DataRow(
+                          title: "Hight 24 Hour",
+                          value: "${coin.hight24Hour} \$",
+                        ),
+                        const SizedBox(height: 6),
+                        _DataRow(
+                            title: "Low 24 Hour",
+                            value: "${coin.low24Hours} \$")
+                      ],
+                    ),
                   ),
                 ],
-              )),
+              ),
             );
           }
           return const Center(child: CircularProgressIndicator());
         },
       ),
+    );
+  }
+}
+
+class _DataRow extends StatelessWidget {
+  const _DataRow({
+    required this.title,
+    required this.value,
+  });
+
+  final String title;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(width: 140, child: Text(title)),
+        const SizedBox(width: 32),
+        Flexible(
+          child: Text(value),
+        ),
+      ],
     );
   }
 }
