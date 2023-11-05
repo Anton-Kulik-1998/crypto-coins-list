b97c29a4185e84453e3c03783d9edf16e6f87b34
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-05 19:43:48 +0300

    Получение ошибок из crypto_list_bloc.dart

--- lib/features/crypto_list/bloc/crypto_list_bloc.dart
+++ lib/features/crypto_list/bloc/crypto_list_bloc.dart
@@ -4,6 +4,8 @@ import 'package:crypto_coins_list/repositories/crypto_coins/abstract_coins_repos
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 part 'crypto_list_event.dart';
 part 'crypto_list_state.dart';
@@ -17,13 +19,19 @@ class CryptoListBloc extends Bloc<CryptoListEvent, CryptoListState> {
         }
         final coinsList = await coinsRepository.getCoinsList();
         emit(CryptoListLoaded(coinsList: coinsList));
-      } catch (e) {
+      } catch (e, st) {
         emit(CryptoListLoadingFailure(exception: e));
+        GetIt.I<Talker>().handle(e, st);
       } finally {
         event.completer?.complete();
       }
     });
   }
   final AbstractCoinsRepository coinsRepository;
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    GetIt.I<Talker>().handle(error, stackTrace);
+  }
 }
- 
