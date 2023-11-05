ef64534af140346ec1dd57aa3a81bb5f1097154d
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-05 18:06:54 +0300

    Создание объекта talker

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,16 @@ import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
+  final talker = TalkerFlutter.init();
+  GetIt.I.registerSingleton(talker);
+  GetIt.I<Talker>().critical("Talker started...");
+  GetIt.I<Talker>().error("Talker started...");
+  GetIt.I<Talker>().debug("Talker started...");
+  GetIt.I<Talker>().info("Talker started...");
+
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
       () => CryptoCoinsRepository(dio: Dio()));
   runApp(const CryptoCoinsListApp());
