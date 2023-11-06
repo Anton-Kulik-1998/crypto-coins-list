84f40bb54069762831862cf30c5be8e6799efe60
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-06 22:17:18 +0300

    Обработка логов Dio

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,29 @@ import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
+import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
   final talker = TalkerFlutter.init();
   GetIt.I.registerSingleton(talker);
   GetIt.I<Talker>().debug("Talker started...");
-  FlutterError.onError = (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
+
+  final dio = Dio();
+  dio.interceptors.add(
+    TalkerDioLogger(
+      talker: talker,
+      settings: const TalkerDioLoggerSettings(
+        printResponseData: false,
+      ),
+    ),
+  );
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
-      () => CryptoCoinsRepository(dio: Dio()));
+      () => CryptoCoinsRepository(dio: dio));
+
+  FlutterError.onError =
+      (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
 
   runZonedGuarded(() => runApp(const CryptoCoinsListApp()), (error, stack) {
     GetIt.I<Talker>().handle(error, stack);
