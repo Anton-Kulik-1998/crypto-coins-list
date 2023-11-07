732ab9ccd9c2d4ebee1f8caac52c12cfd0f34592
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-08 01:46:25 +0300

    Обработка логов Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:crypto_coins_list/crypto_coins_list_app.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
 import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
 import 'package:talker_flutter/talker_flutter.dart';
@@ -23,6 +25,14 @@ void main() {
       ),
     ),
   );
+  Bloc.observer = TalkerBlocObserver(
+    talker: talker,
+    settings: const TalkerBlocLoggerSettings(
+      printEventFullData: false,
+      printStateFullData: false,
+    ),
+  );
+
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
       () => CryptoCoinsRepository(dio: dio));
 
