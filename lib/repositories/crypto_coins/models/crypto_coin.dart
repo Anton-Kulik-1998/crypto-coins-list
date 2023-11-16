cb0ac3d141115904e364565855a59b0c46101f7a
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-16 17:50:26 +0300

    В CryptoCoin добавлен объект CryptoCoinDetail

--- lib/repositories/crypto_coins/models/crypto_coin.dart
+++ lib/repositories/crypto_coins/models/crypto_coin.dart
@@ -1,12 +1,15 @@
+import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin_details.dart';
 import 'package:equatable/equatable.dart';
 
 class CryptoCoin extends Equatable {
-  const CryptoCoin(
-      {required this.name});
+  const CryptoCoin({
+    required this.name,
+    required this.details,
+  });
 
   final String name;
-  
+  final CryptoCoinDetail details;
 
   @override
-  List<Object?> get props => [name];
+  List<Object?> get props => [name, details];
 }
