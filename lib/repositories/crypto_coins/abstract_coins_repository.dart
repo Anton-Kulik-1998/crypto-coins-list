589388432f861fdd1b3d86edee9f0802c845f9b3
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-16 17:53:35 +0300

    Убрал лишний import

--- lib/repositories/crypto_coins/abstract_coins_repository.dart
+++ lib/repositories/crypto_coins/abstract_coins_repository.dart
@@ -1,5 +1,4 @@
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
-import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin_details.dart';
 
 abstract class AbstractCoinsRepository {
   Future<List<CryptoCoin>> getCoinsList();
