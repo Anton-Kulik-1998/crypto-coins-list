701b5c2b799f99589a4a14e03c0416bd037de265
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-16 17:25:30 +0300

    Константы перемещены в класс crypto_coin_details.dart

--- lib/repositories/crypto_coins/models/crypto_coin.dart
+++ lib/repositories/crypto_coins/models/crypto_coin.dart
@@ -2,12 +2,11 @@ import 'package:equatable/equatable.dart';
 
 class CryptoCoin extends Equatable {
   const CryptoCoin(
-      {required this.name, required this.priceInUSD, required this.imageURL});
+      {required this.name});
 
   final String name;
-  final double priceInUSD;
-  final String imageURL;
+  
 
   @override
-  List<Object?> get props => [name, priceInUSD, imageURL];
+  List<Object?> get props => [name];
 }
