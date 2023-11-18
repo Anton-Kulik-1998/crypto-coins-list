ed714f1f6487be922737864ef92adf3d903ef990
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-18 13:52:37 +0300

    Инициализация hive

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 import 'package:talker_dio_logger/talker_dio_logger_interceptor.dart';
 import 'package:talker_dio_logger/talker_dio_logger_settings.dart';
@@ -20,6 +21,8 @@ void main() async {
   final talker = TalkerFlutter.init();
   GetIt.I.registerSingleton(talker);
   GetIt.I<Talker>().debug("Talker started...");
+
+  await Hive.initFlutter();
 
   final app = await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
