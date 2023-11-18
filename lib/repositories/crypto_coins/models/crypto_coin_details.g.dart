b23bf0ef84128ac5f284fdbc0600a07f4ef25119
Антон Кулик <anton.smile.1998@gmail.com>
2023-11-18 12:20:38 +0300

    Генерация Json с учетом исправлений по времени

--- lib/repositories/crypto_coins/models/crypto_coin_details.g.dart
+++ lib/repositories/crypto_coins/models/crypto_coin_details.g.dart
@@ -11,7 +11,7 @@ CryptoCoinDetail _$CryptoCoinDetailFromJson(Map<String, dynamic> json) =>
       priceInUSD: (json['PRICE'] as num).toDouble(),
       imageUrl: json['IMAGEURL'] as String,
       toSymbol: json['TOSYMBOL'] as String,
-      lastUpdate: DateTime.parse(json['LASTUPDATE'] as String),
+      lastUpdate: CryptoCoinDetail._dateTimeFromJson(json['LASTUPDATE'] as int),
       hight24Hour: (json['HIGH24HOUR'] as num).toDouble(),
       low24Hours: (json['LOW24HOUR'] as num).toDouble(),
     );
@@ -19,7 +19,7 @@ CryptoCoinDetail _$CryptoCoinDetailFromJson(Map<String, dynamic> json) =>
 Map<String, dynamic> _$CryptoCoinDetailToJson(CryptoCoinDetail instance) =>
     <String, dynamic>{
       'TOSYMBOL': instance.toSymbol,
-      'LASTUPDATE': instance.lastUpdate.toIso8601String(),
+      'LASTUPDATE': CryptoCoinDetail._dateTimeToJson(instance.lastUpdate),
       'HIGH24HOUR': instance.hight24Hour,
       'LOW24HOUR': instance.low24Hours,
       'PRICE': instance.priceInUSD,
